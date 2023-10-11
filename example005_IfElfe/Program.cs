69021d5db65d5317d1a4dbc01d19dced67162977
Violetta <Violettasokolova957@gmail.com>
2023-10-11 22:01:45 +0300

    example005 if else

---
+++ example005_IfElfe/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите имя пользователя: ");
+string userName = Console.ReadLine();
+
+//if(userName == "Маша")// В данной задаче есть проблема с вводом (имеется некоторое количество вариантов написания имени)
+// Но эта проблема решается командой .ToLower() переводящей все символы нашей строки в нижний регистр
+if(userName.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(userName);
+}
