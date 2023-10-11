e119a0e9f1be4c5536fa34871efae21a6a0debbe
Violetta <Violettasokolova957@gmail.com>
2023-10-11 21:38:40 +0300

    example003 Sum New Metod Random

--- example003_Sum/Program.cs
+++ example003_Sum/Program.cs
@@ -3,8 +3,15 @@
 //Console.WriteLine(numberA + numberB);
 //первый вариант
 
-int numberA = 31;
-int numberB = 10;
-int result = (numberA + numberB);
-Console.WriteLine(result);
+//int numberA = 31;
+//int numberB = 10;
+//int result = (numberA + numberB);
+//Console.WriteLine(result);
 //второй вариант с добавлением переменной
+
+int numberE = new Random().Next(1, 10); // рандомное число от 1 до 9 (10 не включается)
+Console.WriteLine(numberE);
+int numberF = new Random().Next(1, 10);
+Console.WriteLine(numberF);
+int result = numberE + numberF;
+Console.WriteLine(result);
