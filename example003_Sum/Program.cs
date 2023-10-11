2f88d24640c0ebd46f03d13e6d4165338a9f430e
Violetta <Violettasokolova957@gmail.com>
2023-10-11 21:12:15 +0300

    example003_sum

---
+++ example003_Sum/Program.cs
@@ -0,0 +1,10 @@
+﻿//int numberA = 3;
+//int numberB = 10;
+//Console.WriteLine(numberA + numberB);
+//первый вариант
+
+int numberA = 31;
+int numberB = 10;
+int result = (numberA + numberB);
+Console.WriteLine(result);
+//второй вариант с добавлением переменной
